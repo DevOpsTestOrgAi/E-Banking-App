61f5893d590b6225cc4567fbb68662af6c246325
Medelouali <Mohammedelouali1999@gmail.com>
2024-01-04 21:15:33 +0100

    Auth Fixxx

--- CloudGateway/src/main/java/com/ebanking/CloudGateway/CloudGatewayApplication.java
+++ CloudGateway/src/main/java/com/ebanking/CloudGateway/CloudGatewayApplication.java
@@ -3,19 +3,16 @@ package com.ebanking.CloudGateway;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.context.PropertyPlaceholderAutoConfiguration;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import reactor.core.publisher.Mono;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
@@ -25,7 +22,6 @@ public class CloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
 
 	@Bean
 	KeyResolver userKeySolver() {
